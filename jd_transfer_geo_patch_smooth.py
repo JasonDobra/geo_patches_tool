96223b8a264e645620ce57d1bf021539a396787c
JasonDobra <51038636+JasonDobra@users.noreply.github.com>
2026-04-01 09:02:06 +1100

    Update jd_transfer_geo_patch_smooth.py
    refactored

--- jd_transfer_geo_patch_smooth.py
+++ jd_transfer_geo_patch_smooth.py
@@ -16,20 +16,20 @@ def transfer_patch_skinning(selection, skin_transfer_patch, smooth_border, smtAm
         source = selection_name + '_source_set'
         target = selection_name + '_target_set'
         
-        mc.select(source, r=1)
-        mc.select(target, add=1)
+        cmds.select(source, r=1)
+        cmds.select(target, add=1)
         
         source_mesh = selection_name
         
         if skin_transfer_patch:
             smooth_skin_patch(selection_name, target_name, target, source)
         
-        mc.select(cl=True)    
+        cmds.select(cl=True)    
         if smooth_border:
             smooth_edge_border(selection_name, source, target, smtAmt)
         
-        #mc.select(source, r=1)
-        #mc.select(target, add=1)
+        #cmds.select(source, r=1)
+        #cmds.select(target, add=1)
         
         sys.stdout.write('Result:   skin transfer was successful')
         
@@ -38,41 +38,41 @@ def smooth_skin_patch(source_mesh, target_mesh, target, source):
     sourceSkin = mel.eval('findRelatedSkinCluster ' + source_mesh)
     targetSkin = mel.eval('findRelatedSkinCluster ' + target_mesh)
     
-    source_influences = mc.skinCluster(sourceSkin,query=True,inf=True)
-    target_influences = mc.skinCluster(targetSkin,query=True,inf=True)
+    source_influences = cmds.skinCluster(sourceSkin,query=True,inf=True)
+    target_influences = cmds.skinCluster(targetSkin,query=True,inf=True)
     
     for influence in source_influences:
         if influence not in target_influences:
         
-            mc.skinCluster(targetSkin, edit=True, ai=influence, lw=True, ibp=True)
+            cmds.skinCluster(targetSkin, edit=True, ai=influence, lw=True, ibp=True)
             
         else:
             pass
     
-    mc.select(source, r=1)
-    mc.select(target, add=1)
+    cmds.select(source, r=1)
+    cmds.select(target, add=1)
     
-    mc.copySkinWeights(noMirror = True, surfaceAssociation = 'closestPoint', influenceAssociation = 'closestJoint' )
+    cmds.copySkinWeights(noMirror = True, surfaceAssociation = 'closestPoint', influenceAssociation = 'closestJoint' )
     
     
 def smooth_edge_border(selection, source, target, smoothAmt=None):
 
     target_skin = mel.eval('findRelatedSkinCluster ' + selection)
-    target_influences = mc.skinCluster(target_skin, query=True, inf=True)
+    target_influences = cmds.skinCluster(target_skin, query=True, inf=True)
     
-    mc.select(target, add=True)
+    cmds.select(target, add=True)
 
     
-    mc.SelectPolygonSelectionBoundary()
+    cmds.SelectPolygonSelectionBoundary()
     
     smoothAmt = range(smoothAmt)
     
     for amt in smoothAmt:
-        border = mc.GrowPolygonSelectionRegion()
+        border = cmds.GrowPolygonSelectionRegion()
 
     if len(smoothAmt) > 0:
         mel.eval('doSmoothSkinWeightsArgList 3 { "0", "5", "0", "0"   };', ue=True)
         
         
     
-transfer_patch_skinning(mc.ls(sl=True), True, True, 2)
+transfer_patch_skinning(cmds.ls(sl=True), True, True, 2)
